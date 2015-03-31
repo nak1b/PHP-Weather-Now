7e1bb359c8d27ec03f064ab8c89f44ea5e06334b
Nakib Momin <nbmomin14@gmail.com>
2015-03-31 19:06:58 -0400

    Added html and style

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Contact Form</title>
+	<title>Weather Now</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="js/jquery.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 center">
+				<h1 class="heading">Weather Now</h1>
+				<p class="lead heading">Enter your city below to get a forecast</p>
+
+				<div class="form">
+					<div class="form-group">
+						 <input type="text" class="form-control" name="city" id="city" placeholder="Eg: Toronto, New York, Paris...">
+					</div>
+
+					<button class="btn btn-success btn-lg">Get My Weather</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
